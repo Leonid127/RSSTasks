f56a6de5872ac723a049cd1980f2f609390cdff6
Leonid127 <solodkov127@gmail.com>
2022-02-14 22:45:08 +0500

    feat: finish adaptive and burger

---
+++ portfolio/index.js
@@ -0,0 +1,27 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.header-nav');
+const lineOne = document.querySelector('.line-one');
+const lineTwo = document.querySelector('.line-two');
+const lineThree = document.querySelector('.line-three');
+
+const toggleMenu = () => {
+    menu.classList.toggle('open');
+    lineOne.classList.toggle('open');
+    lineTwo.classList.toggle('open');
+    lineThree.classList.toggle('open');
+}
+
+burger.addEventListener('click', toggleMenu);
+
+
+function closeMenu (event) {
+    if (event.target.classList.contains('header-link')) {
+        burger.classList.remove('open');
+        menu.classList.remove('open');
+        lineOne.classList.remove('open');
+        lineTwo.classList.remove('open');
+        lineThree.classList.remove('open');
+    }
+}
+
+menu.addEventListener('click', closeMenu);
