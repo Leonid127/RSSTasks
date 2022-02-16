b105aeea65174c2e960c971e7d50e79dbf4e0cf6
Leonid127 <solodkov127@gmail.com>
2022-02-16 18:23:09 +0500

    feat: finish change img

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,3 +1,5 @@
+//************************************************* */ Burger menu
+
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.header-nav');
 const lineOne = document.querySelector('.line-one');
@@ -26,4 +28,44 @@ function closeMenu (event) {
 
 menu.addEventListener('click', closeMenu);
 
-// Commit example
+// *********************************************** Changing img
+
+const portfolioBtn = document.querySelectorAll('.portfolio-btn');
+const portfolioImg = document.querySelectorAll('.portfolio-img');
+const portfolioBtns = document.querySelector('.portfolio-buttons');
+
+// portfolioImg.forEach((img, index) => img.src = `./assets/img/winter/${index + 1}.jpg`);
+
+function changeImage(event) {
+    if(event.target.classList.contains('portfolio-btn')) {
+        portfolioImg.forEach((img, index) => {
+        img.src = `./assets/img/${event.target.dataset.season}/${index + 1}.jpg`;
+    });
+}
+}
+portfolioBtns.addEventListener('click', changeImage);
+
+//**************************************************** кэширование
+
+const seasons = ['winter', 'spring', 'summer', 'autumn'];
+
+function preloadImages() {
+    seasons.forEach((image) => {
+    for(let i = 1; i <= 6; i++) {
+      const img = new Image();
+      img.src = `./assets/img/${image}/${i}.jpg`;
+    }
+  })
+}
+
+preloadImages();
+
+
+const changeClassActive = (event) => {
+    portfolioBtn.forEach((el) => {
+        el.classList.remove('active');
+    });
+    event.target.classList.add('active')
+}
+
+portfolioBtns.addEventListener('click', changeClassActive);
